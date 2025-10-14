0255f2348eba1b146e205331c2771b1bc2b0e4d9
Cao, Zhong Z <zhong.z.cao@intel.com>
2025-10-14 18:03:45 +0800

    Add more scratch tests
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CXX=clang++
 CXXFLAGS=-g3 -O0 -ggdb3 -fno-inline -fno-omit-frame-pointer --std=c++17 -Icutlass/include -Icutlass/tools/util/include -Iinclude
 
-all : layout_test
+all : coalesce_test composition_test
 
 clean:
-	rm -f layout_test
+	rm -f coalesce_test composition_test
--- coalesce_test.cpp
+++ coalesce_test.cpp
@@ -4,14 +4,17 @@
 using namespace cute;
 
 int main() {
-  auto a = Layout<Shape <_2,Shape <_1,_6>>,
-                  Stride<_1,Stride<_6,_2>>>{};
+  auto a = Layout<Shape <_1, Shape <_1,_6>, _1>,
+                  Stride<_3, Stride<_6,_2>, _2>>{};
   auto result = coalesce(a, Step<_1,_1>{});   // (_2,_6):(_1,_2)
   // Identical to
   auto same_r = make_layout(coalesce(layout<0>(a)),
                             coalesce(layout<1>(a)));
 
-  std::cout << "Result layout: " << same_r <<std::endl;
+  std::cout << "Result layout: " << result <<std::endl;
+
+  auto another = coalesce(a, Step<_1, _1, _1>{});
+  std::cout << "Result layout: " << another <<std::endl;
 
   return 0;
 }
