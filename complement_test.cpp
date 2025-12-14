cf359d66742ad52bf1e60460e2856fc24d0d54d6
Cao, Zhong Z <zhong.z.cao@intel.com>
2025-12-14 11:19:00 +0800

    Create a arbitrary case to understand complement
    Signed-off-by: Cao, Zhong Z <zhong.z.cao@intel.com>

--- complement_test.cpp
+++ complement_test.cpp
@@ -10,8 +10,25 @@ int main() {
   std::cout << "It's coshape is " << coshape(a) <<std::endl;
   std::cout << "It's cosize is " << cosize(a) << std::endl;
 
-  auto b = Layout<Shape <_4>, Stride<_1>> {};
-  std::cout << "Complement is "<< complement(b, Int<14>{}) << std::endl;
+  auto b = Layout<Shape <_4>, Stride<_2>> {};
+  std::cout << "Cosize of "<<b<<" is "<<cosize(b)<<std::endl;
+  std::cout << "Complement "<<b<<" of 24 is "<< complement(b, Int<24>{}) << std::endl;
+
+  auto c = Layout<Shape <_6>, Stride<_4>> {};
+  std::cout << "Composition " << b <<" and "<< c <<" is "<<composition(b, c) <<std::endl;
+
+  {
+    auto a = Layout<Shape <_2, _2>, Stride<_1, _6>> {};
+    constexpr int c = 24;
+    std::cout << "Compliment "<< a <<" with "<< c <<" is "<<complement(a, c)<<std::endl;
+  }
+
+  {
+    auto a = Layout<Shape <_2, _2>, Stride<_2, _6>> {};
+    constexpr int c = 24;
+    std::cout << "This case show algorithm is best effort, not complete" <<std::endl;
+    std::cout << "Compliment "<< a <<" with "<< c <<" is "<<complement(a, c)<<std::endl;
+  }
 
   return 0;
 }
